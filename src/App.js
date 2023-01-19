fb33156773c190fcbcaeef51cf79e4fa1133ac67
teke <mutau85@gmail.com>
2023-01-19 14:11:10 +0200

    Implement routing functionality to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+/* eslint-disable react/prefer-stateless-function */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Calculator from './components/Calculator';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
-    return <Calculator />;
+    return (
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </Router>
+    );
   }
 }
 
---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Quote.css';
+
+const Quote = () => (
+  <div className="quoteInformation">
+    <p className="fs-1 quote text-center">
+      Mathematics is not about numbers, equations, computaions, or algorithms:
+      <br />
+      it is about understanding. -William Paul Thurston
+    </p>
+  </div>
+);
+
+export default Quote;
